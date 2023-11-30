1a9201f27aefbbf0f3826b0293649e90369d5607
cnrgrsc <cnrgrsc@gmail.com>
2023-11-30 23:29:05 +0300

    test yazılacak

--- Project.Circiut/Controllers/DataController.cs
+++ Project.Circiut/Controllers/DataController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Project.Circiut.Elasticsearch.Abstract;
 using Project.Circiut.Models;
 using Project.Circiut.Redis.Abstract;
+using Project.Circiut.Utilities;
 
 namespace Project.Circiut.Controllers
 {
@@ -12,31 +13,50 @@ namespace Project.Circiut.Controllers
 	{
 		private readonly IRedisService _redisService;
 		private readonly IElasticsearchService _elasticsearchService;
+		private readonly ILogger<DataController> _logger;
 
-		public DataController(IRedisService redisService, IElasticsearchService elasticsearchService)
+		public DataController(IRedisService redisService, IElasticsearchService elasticsearchService, ILogger<DataController> logger)
 		{
 			_redisService = redisService;
 			_elasticsearchService = elasticsearchService;
+			_logger = logger;
 		}
 
 		[HttpPost]
 		[Route("write")]
 		public async Task<IActionResult> WriteData([FromBody] ProductData data)
 		{
-			// Redis'e veri yazma
 			var redisKey = $"product:{data.Id}";
-			await _redisService.SetAsync(redisKey, data.ToJson());
 
-			// Elasticsearch'e veri yazma
-			var result = await _elasticsearchService.IndexDocumentAsync("products", data.Id.ToString(), data);
-
-			if (result)
+			try
 			{
-				return Ok(new { Message = "Data written to both Redis and Elasticsearch" });
+				// Redis'e veri yazmayı dene
+				await _redisService.SetAsync(redisKey, data.ToJson());
+				return Ok(new { Message = "Data written to Redis" });
 			}
-			else
+			catch (Exception ex)
 			{
-				return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Data writing to Elasticsearch failed" });
+				_logger.LogError(ex, "Failed to write data to Redis, attempting to write to Elasticsearch.");
+				try
+				{
+					// Redis'e yazma başarısız oldu, Elasticsearch'e yaz
+					var result = await _elasticsearchService.IndexDocumentAsync("products", data.Id.ToString(), data);
+					if (result)
+					{
+						return Ok(new { Message = "Data written to Elasticsearch" });
+					}
+					else
+					{
+						_logger.LogError("Failed to write data to Elasticsearch.");
+						return StatusCode(StatusCodes.Status503ServiceUnavailable, GetFallbackUtilities.GetFallbackResults());
+					}
+				}
+				catch (Exception exElastic)
+				{
+					// Elasticsearch'e yazma da başarısız oldu
+					_logger.LogError(exElastic, "Failed to write data to both Redis and Elasticsearch.");
+					return StatusCode(StatusCodes.Status503ServiceUnavailable, GetFallbackUtilities.GetFallbackResults());
+				}
 			}
 		}
 	}
---
+++ Project.Circiut/Utilities/GetFallbackUtilities.cs
@@ -0,0 +1,28 @@
+﻿namespace Project.Circiut.Utilities
+{
+	public  static class GetFallbackUtilities
+	{
+
+		public static string GetFallbackResults()
+		{
+			return "{ \"message\": \"Service is currently unavailable. Please try again later.\" }";
+		}
+		//public static async Task<T> GetFallbackAsync<T>(Func<Task<T>> action, Func<Task<T>> fallbackAction, ILogger logger)
+		//{
+		//	try
+		//	{
+		//		return await action();
+		//	}
+		//	catch (BrokenCircuitException bce)
+		//	{
+		//		logger.LogWarning(bce, "Circuit Breaker is open. Falling back to fallback action.");
+		//		return await fallbackAction();
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		logger.LogError(ex, "An error occurred while accessing Redis.");
+		//		return await fallbackAction();
+		//	}
+		//}
+	}
+}
